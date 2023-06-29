f132e475eb95ab2009ae990d1e04b68f8491c6fc
marvim1990 <marcosvinim.cruz@gmail.com>
2023-06-29 11:36:21 -0300

    test

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -8,7 +8,7 @@ import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionRoutes(app: FastifyInstance) {
   app.get('/teste', async (req, res) => {
-    return { message: 'okk' }
+    return { message: 'ok' }
   })
   app.get(
     '/',
